bf44932b01be67008126be4a8fe8f339330723e0
Deepbaran <deepbaran97@gmail.com>
2022-08-10 23:29:31 +0530

    Basic JS Notes

---
+++ 02-Basics/01-variables.js
@@ -0,0 +1,20 @@
+var fullName = "Deepbaran Kar";
+
+// how to name variables
+
+// fullName
+// full-name
+// full_name
+// FullName
+
+var courseName = "React JS Bootcamp"; //String Literals
+
+var isLoggedIn = true;
+
+var loggedCount = 34;
+
+var paymentMode;
+console.log(paymentMode);
+
+paymentMode = "Credit Card";
+console.log(paymentMode);
